7d13415bab19cf36cbdafc916ff34beff9727715
Prasad Patil <prpatil42@gmail.com>
2014-12-08 18:27:29 -0500

    comments for tspreg_report and namespace update

--- R/tspreg_report.R
+++ R/tspreg_report.R
@@ -2,8 +2,12 @@
 #' Generate a knitr report for model-building using tspreg
 #'
 #' @param data A matrix of continuous data with features in rows
-#' and samples in columns
-#' @param outcome A binary outcome vector with length = ncol(data)
+#' and samples in columns (m x n) to train a model. Rownames must be provided.
+#' @param outcome A binary outcome vector of length n
+#' @param covar A matrix of covariates to adjust for (n x p)
+#' @param val A matrix similar to data on which to validate the trained model.
+#' @param val_outcome The binary outcome vector for val
+#' @param val_covar The covariate matrix for val
 #' @param npair The number of pairs desired in the final model
 #' @filepath A character string of where the report should be generated.
 #' Default NULL, which generates a file called "{curtime}_output.Rmd" in the
